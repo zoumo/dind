7a340019ed0a49bdf31b50ee122919d86cf6be4e
Jim Zhang <jim.zoumo@gmail.com>
2016-11-15 12:07:25 +0800

    use golang

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@ set -x
 set -e
 
 # pre build
-docker pull cargo.caicloud.io/caicloud/golang
+docker pull golang
 # docker load -i script/golang.tar
 git clone https://github.com/zoumo/go_test.git $PWD/go_test
-docker run --rm -v $PWD/go_test:/go/src/github.com/zoumo/go_test -w /go/src/github.com/zoumo/go_test cargo.caicloud.io/caicloud/golang sh build.sh
+docker run --rm -v $PWD/go_test:/go/src/github.com/zoumo/go_test -w /go/src/github.com/zoumo/go_test golang sh build.sh
 
 # build
 docker build -t integration $PWD/go_test
